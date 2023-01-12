dc5e798be2b98ca1319d456ec9baa82d25d8a6b1
martinorue <53911512+martinorue@users.noreply.github.com>
2023-01-12 15:37:00 -0300

    - show error only when pressing submit - add color variables

--- form-validation.js
+++ form-validation.js
@@ -6,20 +6,20 @@
     const email = document.getElementById('email');
     const emailError = document.getElementById('email_error');
 
-    email.addEventListener('input', function (event) {
-      // Each time the user types something, we check if the
-      // form fields are valid.
+    // email.addEventListener('input', function (event) {
+    //   // Each time the user types something, we check if the
+    //   // form fields are valid.
 
-      if (email.validity.valid) {
-        // In case there is an error message visible, if the field
-        // is valid, we remove the error message.
-        emailError.innerHTML = ''; // Reset the content of the message
-        emailError.className = 'error'; // Reset the visual state of the message
-      } else {
-        // If there is still an error, show the correct error
-        showError();
-      }
-    });
+    //   if (email.validity.valid) {
+    //     // In case there is an error message visible, if the field
+    //     // is valid, we remove the error message.
+    //     emailError.innerHTML = ''; // Reset the content of the message
+    //     emailError.className = 'error'; // Reset the visual state of the message
+    //   } else {
+    //     // If there is still an error, show the correct error
+    //     showError();
+    //   }
+    // });
 
     form.addEventListener('submit', function (event) {
       // if the form contains valid data, we let it submit
